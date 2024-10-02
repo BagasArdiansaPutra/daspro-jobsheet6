05199f4094b4195dcdef2c95e52aeb039595d3dd
BagasArdiansaPutra <bagasardiansa06.com>
2024-10-02 11:18:47 +0700

    haihai

--- Pemilihan2Percobaan106.java
+++ Pemilihan2Percobaan106.java
@@ -3,11 +3,11 @@ public  class Pemilihan2Percobaan106 {
 
     public static void main(String[] args) {
         Scanner sc06 = new Scanner(System.in);
-
         System.out.print("Masukkan Tahun: ");
         int tahun = sc06.nextInt();
+
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0) {
+            if ((tahun % 100) == 0) {
                 if ((tahun % 400) == 0 ) {
                     System.out.println("Tahun kabisat");
                 } else {
