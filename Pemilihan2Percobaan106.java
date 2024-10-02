8b9876e3554d7577d24ab8276522d20f52a4346f
BagasArdiansaPutra <bagasardiansa06.com>
2024-10-02 09:42:49 +0700

    hfdwgyg

---
+++ Pemilihan2Percobaan106.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public  class Pemilihan2Percobaan106 {
+
+    public static void main(String[] args) {
+        Scanner sc06 = new Scanner(System.in);
+
+        System.out.print("Masukkan Tahun: ");
+        int tahun = sc06.nextInt();
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0) {
+                System.out.println("Tahun Kabisat");
+        } else 
+        System.out.println("Bukan Tahun Kabisat");
+
+            }
+        }  
+    }
